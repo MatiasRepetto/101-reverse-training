916be2e0ad37b64f0b19d178a78e92ce5c98c37c
MatiasRepetto <repetto.matias3@gmail.com>
2022-08-24 18:41:29 -0300

    basic program to test things in debugger

--- Function-Hooking-Code-Cave/toCrack&HookProgram.cpp
+++ Function-Hooking-Code-Cave/toCrack&HookProgram.cpp
@@ -1,13 +1,15 @@
 #include <stdio.h>
 #include <iostream>
+#include <string>
+#include <cstdlib>
 
 int main(){
-    std::string allow = "momo123";
+    std::string allow = std::to_string(rand());
     std::string password;
 
     std::cout << "Increse su password " << std::endl;
     std::cin >> password;
-    if(password == allow){
+    if(password == allow){ // no way to hit the pass crack it !
         std::cout << "Ingresado correctamente" << std::endl;
     }else{
         std::cout << "Error contraseña incorrecta" << std::endl;
